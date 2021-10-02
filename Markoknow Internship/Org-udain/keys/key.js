742554e8428be5d096eb34ebf4a47c2d935da2b9
Udain Srivastava <63836083+Udain123@users.noreply.github.com>
2021-10-02 16:05:56 +0530

    Delete Markoknow Internship/Org-udain directory

--- Markoknow Internship/Org-udain/keys/key.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config("../.env");
-
-module.exports = {
-  MONGOURI: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ic0bh.mongodb.net/${process.env.DB_DATABSE}?retryWrites=true&w=majority`,
-  JWT_SECRET: process.env.JWT_SECRET,
-};
--- Markoknow Internship/Org-udain/public/src/components/validateContact.js
+++
@@ -1,6 +0,0 @@
-function validateContact(contact) {
-    var re = /^[6-9]\d{9}$/;
-    return re.test(String(contact).toLowerCase());
-  }
-  
-  export { validateContact };
